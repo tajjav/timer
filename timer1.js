c5a314b8c1f502793f182d48d475a7f071a574fb
tajjav <taj.jav@hotmail.com>
2023-10-16 23:30:53 -0600

    this commit has complete code for the timer.js to work in node REPL

--- timer1.js
+++ timer1.js
@@ -1,13 +1,27 @@
-const readline = require('readline').createInterface({
-  input: process.stdin,
-  output: process.stdout,
-});
-readline.question(`Please enter the time in seconds for Timer?`, inputTime => {
-  
- 
-  console.log(`Hi ${inputTime}!`);
-  readline.close();
-});
-const timer = function(args) {
-  process.stdout.write('\x07');
+const readline = require('readline');
+
+/**
+ * timer function definition, it works on node REPL.
+ */
+const timer = function () {
+  //create interface for console input/output
+  const rI = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+  });
+  rI.question(`Please enter the time in seconds for the buzzer time, use space for multiple buzzer times?`, (inputTime) => {
+    //split single string array from rI.history
+    const splitInputTime = rI.history.map((e) => e.split(' '));
+    const splitInputTimeArray = splitInputTime[0];
+    //loop over the array and set element as setTimeout wait time
+    splitInputTimeArray.forEach(element => {
+      setTimeout(() => {
+        //buzzer
+        process.stdout.write('\x07');
+      }, parseInt(element));
+    });
+    //closes interface
+    rI.close();
+  });
 }
+timer();
